684c685acdb12b53bb8b56b972f44440e78aae02
Angel Lopez <angel.finlandia@gmail.com>
2016-11-09 16:29:32 +0200

    Added gulpif and minisfied production javascript

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var gulp = require('gulp'),
 	compass = require('gulp-compass'),
 	browserify = require('gulp-browserify'),
 	connect = require('gulp-connect'),
+	gulpif = require('gulp-if'),
+	uglify = require('gulp-uglify'),
 	concat = require('gulp-concat');
 
 
@@ -50,6 +52,7 @@ gulp.task('js', function() {
 	gulp.src(jsSources)
 		.pipe(concat('script.js'))
 		.pipe(browserify())
+		.pipe(gulpif(env === 'production', uglify()))
 		.pipe(gulp.dest(outputDir + 'js'))
 		.pipe(connect.reload())
 
